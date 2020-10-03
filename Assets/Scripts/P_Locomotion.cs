4d7198daa9db9a4be13a81cf7d87c16189269ecb
Busldii <clementmaubert22@gmail.com>
2020-10-03 11:28:38 +0200

    Ajout nouvelle scene + fps + custom skybox

---
+++ Assets/Scripts/P_Locomotion.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Locomotion : MonoBehaviour
+{
+    public bool m_isGrounded;
+
+    public float m_speed = 12f;
+    public float m_jumpHeight = 3f;
+    public float m_gravity = -9.81f;
+    public float m_groundDistance = 0.5f;
+
+    public Transform m_groundCheck;
+    public LayerMask m_groundMask;
+
+    Vector2 m_moveInput;
+    Vector3 m_velocity;
+
+    CharacterController m_characterController;
+
+    void Start()
+    {
+        m_characterController = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //check if is grounded
+
+        m_isGrounded = Physics.CheckSphere(m_groundCheck.position, m_groundDistance, m_groundMask);
+
+        if(m_isGrounded && m_velocity.y < 0)
+        {
+            m_velocity.y = -2f;
+        }
+
+        //inputs
+
+        m_moveInput.x = Input.GetAxis("Horizontal");
+        m_moveInput.y = Input.GetAxis("Vertical");
+
+        //move
+
+        Vector3 m_move = transform.right * m_moveInput.x + transform.forward * m_moveInput.y;
+
+        m_characterController.Move(m_move * m_speed * Time.deltaTime);
+
+        //jump
+
+        if (Input.GetButtonDown("Jump") && m_isGrounded)
+        {
+            m_velocity.y = Mathf.Sqrt(m_jumpHeight * -2f * m_gravity);
+        }
+
+        //gravity
+
+        m_velocity.y += m_gravity * Time.deltaTime;
+        m_characterController.Move(m_velocity * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/P_Look.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class P_Look : MonoBehaviour
+{
+   [Range(0, 300)]
+    public float m_mouseSensitivity = 100f;
+    public Transform p_body;
+
+    Vector2 m_mouse;
+    float m_xRotation = 0f;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        m_mouse.x = Input.GetAxis("Mouse X") * m_mouseSensitivity * Time.deltaTime;
+        m_mouse.y = Input.GetAxis("Mouse Y") * m_mouseSensitivity * Time.deltaTime;
+
+        m_xRotation -= m_mouse.y;
+        m_xRotation = Mathf.Clamp(m_xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(m_xRotation, 0f, 0f);
+
+        p_body.Rotate(Vector3.up * m_mouse.x);
+    }
+}
