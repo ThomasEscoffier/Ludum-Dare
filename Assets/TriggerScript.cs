aca1b0e605b398027185ef2313f98a11f07fcfbf
Thomas <a.thomasescoffier@gmail.com>
2020-10-03 20:54:12 +0200

    Animation control / Map placeholders Done

---
+++ Assets/TriggerScript.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerScript : MonoBehaviour
+{
+    public Animator animator;
+    public Collider triggerZone;
+    private bool triggered;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(triggered)
+        {
+
+            if (Input.GetKeyDown(KeyCode.E))
+            {
+                animator.enabled = true;
+
+                if (animator.GetBool("isOpening") == false)
+                {
+                   animator.SetBool("isOpening", true);
+                }
+                else
+                {
+                    animator.SetBool("isOpening", false);
+                }
+            }
+
+
+            if (Input.GetKeyDown(KeyCode.Q))
+            {
+
+                if (animator.enabled == true)
+                {
+                    animator.enabled = false;
+                }
+                else
+                {
+                    animator.enabled = true;
+                }
+            }
+        }
+
+    }
+
+    private void OnTriggerEnter(Collider triggerZone)
+    {
+        triggered = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        triggered = false;
+    }
+}
